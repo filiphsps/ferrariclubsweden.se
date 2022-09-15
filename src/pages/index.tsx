4a36a86f6d355ffc71615d2f28c118ac271df066
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2022-09-15 21:09:51 +0200

    chore(meta): Initial commit.

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from 'react';
+
+interface IndexPageProps {}
+const IndexPage: FunctionComponent<IndexPageProps> = () => {
+    return <h1>Hello world!</h1>;
+};
+
+export default IndexPage;
